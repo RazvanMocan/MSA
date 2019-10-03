aa7bcc5a953149c39d9ae3da81f052295e46d9f5
Razvan Mocan <razvanmocan15@gmail.com>
2019-10-03 15:42:29 +0300

    Add login template

---
+++ app/src/main/java/com/mocan/autoreflex/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.mocan.autoreflex.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
