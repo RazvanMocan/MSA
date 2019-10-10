da128247f8d24ed64032ed4dbceb3c1d67c9201d
Razvan Mocan <razvanmocan15@gmail.com>
2019-10-10 18:27:29 +0300

    Add DRPCIV exam simulation

--- app/src/main/java/com/mocan/autoreflex/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/mocan/autoreflex/ui/gallery/GalleryFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mocan.autoreflex.R
+import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
+
 
 class GalleryFragment : Fragment() {
 
@@ -22,10 +24,25 @@ class GalleryFragment : Fragment() {
         galleryViewModel =
             ViewModelProviders.of(this).get(GalleryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(this, Observer {
-            textView.text = it
+
+        val browser:WebView = root.findViewById(R.id.examView)
+
+        // Enable javascript
+        browser.settings.setJavaScriptEnabled(true)
+
+        // Set WebView client
+        browser.webChromeClient = WebChromeClient()
+
+        browser.webViewClient = (object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return true
+            }
         })
+        // Load the webpage
+        browser.loadUrl("https://www.drpciv.ro/dlexam")
+
         return root
     }
 }
