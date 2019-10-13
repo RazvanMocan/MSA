362d98137bddedde47cc735ca672d5b601578dbe
Razvan Mocan <razvanmocan15@gmail.com>
2019-10-13 18:12:49 +0300

    Upload ID capability

--- app/src/main/java/com/mocan/autoreflex/ui/signup/IDFragment.kt
+++ app/src/main/java/com/mocan/autoreflex/ui/signup/IDFragment.kt
@@ -1,13 +1,26 @@
 package com.mocan.autoreflex.ui.signup
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.mocan.autoreflex.R
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.mocan.autoreflex.ui.login.LoginViewModel
+import com.mocan.autoreflex.ui.login.LoginViewModelFactory
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +40,10 @@ class IDFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
+    private val READ_REQUEST_CODE: Int = 42
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var mStorageRef: StorageReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +51,14 @@ class IDFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        loginViewModel = ViewModelProviders.of(this,
+            LoginViewModelFactory()
+        )
+            .get(LoginViewModel::class.java)
+        mStorageRef = FirebaseStorage.getInstance().reference
+
+
+        checkFilePermissions()
     }
 
     override fun onCreateView(
@@ -41,61 +66,133 @@ class IDFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_id, container, false)
+        val root = inflater.inflate(R.layout.fragment_id, container, false)
+        val searchBtn = root.findViewById<Button>(R.id.button3)
+
+        searchBtn.setOnClickListener { performFileSearch() }
+
+        return root
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
+    fun performFileSearch() {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            // Filter to only show results that can be "opened", such as a
+            // file (as opposed to a list of contacts or timezones)
+            addCategory(Intent.CATEGORY_OPENABLE)
+
+            // Filter to show only images, using the image MIME data type.
+            // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+            // To search for all documents available via installed storage providers,
+            // it would be "*/*".
+            type = "image/*"
+        }
+        Log.w("Sunt aici", "acum cu voi")
+        startActivityForResult(intent, READ_REQUEST_CODE)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        Log.e("Next", "aici")
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            resultData?.data?.also { uri ->
+                Log.e("Te rog", "mergi")
+                uploadFile(uri)
+            }
         }
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
+    private fun uploadFile(uri: Uri) {
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            Log.e("Te rog", "mergi 3")
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment IDFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            IDFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+            val userID = user.uid
+            val storageReference = mStorageRef.child("new_users/$userID.jpg")
+            storageReference.putFile(uri).addOnCompleteListener{task ->
+                if (task.isSuccessful) {
+                    Log.e("file uploaded", "Success!")
                 }
             }
+
+        }
+
     }
+
+private fun checkFilePermissions() {
+    var permissionCheck = checkSelfPermission(context!!, "Manifest.permission.READ_EXTERNAL_STORAGE")
+    permissionCheck += checkSelfPermission(context!!, "Manifest.permission.WRITE_EXTERNAL_STORAGE")
+    if (permissionCheck != 0) {
+        this.requestPermissions(
+            arrayOf(
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ), 1001
+        ) //Any number
+    }
+}
+
+// TODO: Rename method, update argument and hook method into UI event
+fun onButtonPressed(uri: Uri) {
+    listener?.onFragmentInteraction(uri)
+}
+
+override fun onAttach(context: Context) {
+    super.onAttach(context)
+    if (context is OnFragmentInteractionListener) {
+        listener = context
+    } else {
+        throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+    }
+}
+
+override fun onDetach() {
+    super.onDetach()
+    listener = null
+}
+
+/**
+ * This interface must be implemented by activities that contain this
+ * fragment to allow an interaction in this fragment to be communicated
+ * to the activity and potentially other fragments contained in that
+ * activity.
+ *
+ *
+ * See the Android Training lesson [Communicating with Other Fragments]
+ * (http://developer.android.com/training/basics/fragments/communicating.html)
+ * for more information.
+ */
+interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    fun onFragmentInteraction(uri: Uri)
+}
+
+companion object {
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment IDFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    @JvmStatic
+    fun newInstance(param1: String, param2: String) =
+        IDFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_PARAM1, param1)
+                putString(ARG_PARAM2, param2)
+            }
+        }
+}
 }
