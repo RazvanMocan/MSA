2a115c9546c1abcb13cce991daecb9c1c906aa7e
Razvan Mocan <razvanmocan15@gmail.com>
2020-01-18 23:15:08 +0200

    A little improvement to list layouts

--- app/src/main/java/com/mocan/autoreflex/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mocan/autoreflex/ui/login/LoginActivity.kt
@@ -195,7 +195,6 @@ class LoginActivity : AppCompatActivity() {
         val myIntent = Intent(this, activity)
         myIntent.putExtra("type", type)
         myIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-        dialog.dismiss()
 
         startActivity(myIntent)
 
